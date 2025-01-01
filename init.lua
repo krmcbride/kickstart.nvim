a1a476eb62a9b1bd703b2316c7303743002e71cb
Kevin McBride <681929+krmcbride@users.noreply.github.com>
2024-12-31 16:45:03 -0800

    init/treesitter: update languages

--- init.lua
+++ init.lua
@@ -924,7 +924,26 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'terraform', 'hcl' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'go',
+        'hcl',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'terraform',
+        'typescript',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
